29cfd2826e1480f92ffea8771e1a475e8550350c
muni <booscarface@gmail.com>
2023-07-09 10:31:45 +0200

    get/set

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,42 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        var result = abstractedLogic();
+        var record = new Coordinates(0,0,0); 
+        var object = new ThreeDCalculator(record);
+        System.out.println(result);
+        System.out.println(record);
+        System.out.println(object);
     }
+
+    public static int abstractedLogic() {
+
+        // logic
+        return 1;
+    }
+
+    
+
+}
+
+record Coordinates(int x, int y, int z) {
+
+    }
+
+class ThreeDCalculator {
+    private Coordinates origin;
+
+    public ThreeDCalculator(Coordinates origin) {
+        this.origin = origin;   
+    }
+
+    public Coordinates getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Coordinates origin) {
+        this.origin = origin;
+    }
+
 }
