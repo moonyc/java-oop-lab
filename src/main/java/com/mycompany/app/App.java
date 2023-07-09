78fe1ac3fea9fed397ccb339deec11e848fea38b
muni <booscarface@gmail.com>
2023-07-09 11:47:38 +0200

    polymorphism

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,11 +9,11 @@ public class App {
     public static void main( String[] args ) {
         var result = abstractedLogic();
         var record = new Coordinates(0,0,0); 
-        var object = new ThreeDCalculator(record);
+        var object = new ThreeDTranslation(record);
         System.out.println(result);
         System.out.println(record);
         System.out.println(object);
-        System.out.println(object.translate( new Coordinates(1, 2, 3)));
+        System.out.println(object.transform( new Coordinates(1, 2, 3)));
         System.out.println(logAndReturn(object));
     }
 
@@ -46,26 +46,45 @@ record Coordinates(int x, int y, int z) {
 
     }
 
-class ThreeDCalculator {
+// Inheritance: two ways
+// 1
+@FunctionalInterface
+interface ThreeDTransform {
+    Coordinates transform(Coordinates toTransform);
+
+    default Coordinates transformDefaultOrigin() {
+        return transform( new Coordinates(0, 0, 0));
+    }
+}
+
+// 2 
+
+abstract class OriginAware {
     private Coordinates origin;
 
-    public ThreeDCalculator(Coordinates origin) {
-        this.origin = origin;   
+    public OriginAware(Coordinates origin) {
+        this.origin = origin;
     }
 
     public Coordinates getOrigin() {
-        return origin;
+        return this.origin;
     }
 
     public void setOrigin(Coordinates origin) {
         this.origin = origin;
     }
+}
+class ThreeDTranslation extends OriginAware implements ThreeDTransform {
 
-    public Coordinates translate(Coordinates transform) {
+    public ThreeDTranslation(Coordinates origin) {
+        super(origin);
+    }
+
+    public Coordinates transform(Coordinates transform) {
         return new Coordinates(
-            origin.x() + transform.x(),
-            origin.y() + transform.y(),
-            origin.z() + transform.z()
+            getOrigin().x() + transform.x(),
+            getOrigin().y() + transform.y(),
+            getOrigin().z() + transform.z()
         );
 
     }
