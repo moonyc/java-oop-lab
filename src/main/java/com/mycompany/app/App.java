b71efcbfc5cf4b31d111cce330c94f4ded539edd
muni <booscarface@gmail.com>
2023-07-09 11:08:17 +0200

    generics example

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,10 @@
 package com.mycompany.app;
 
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+
 public class App {
     public static void main( String[] args ) {
         var result = abstractedLogic();
@@ -8,6 +13,8 @@ public class App {
         System.out.println(result);
         System.out.println(record);
         System.out.println(object);
+        System.out.println(object.translate( new Coordinates(1, 2, 3)));
+        System.out.println(logAndReturn(object));
     }
 
     public static int abstractedLogic() {
@@ -16,7 +23,22 @@ public class App {
         return 1;
     }
 
-    
+    public static <T> T logAndReturn(T obj) {
+        Logger.getAnonymousLogger().log( new LogRecord( Level.INFO, obj.toString()));
+        return obj;
+    }
+
+    // supplier : takes nothing and returns T
+    // consumer : takes T and returns nothing
+
+    public static <T> Supplier<T> decorateWithLogger(Supplier<T> functionToDecorate) {
+        return () -> {
+            Logger.getAnonymousLogger().info(
+                functionToDecorate.toString()
+            );
+            return functionToDecorate.get();
+        };
+    }
 
 }
 
@@ -37,6 +59,15 @@ class ThreeDCalculator {
 
     public void setOrigin(Coordinates origin) {
         this.origin = origin;
+    }
+
+    public Coordinates translate(Coordinates transform) {
+        return new Coordinates(
+            origin.x() + transform.x(),
+            origin.y() + transform.y(),
+            origin.z() + transform.z()
+        );
+
     }
 
 }
